1e484c880b968a851977b2bc26f1036806703fc7
Nelo Pauselli <npauselli@trabajo.gob.ar>
2021-06-05 22:55:47 -0300

    (re)cargando automáticamente cuando cambia la dirección

--- www/js/bundle.js
+++ www/js/bundle.js
@@ -30,7 +30,8 @@ function Staked(pid, wantTokenName, staked) {
 }
 
 function ViewModel() {
-    this.address = '0x5a31925d4d8bed0abd2b3e452644691be8739c67';
+    var self = this;
+    this.address = ko.observable();
 
     this.pools = [];
     this.pools.push(new Pool(null, 'Pancake Swap', '/img/pancake.png'));
@@ -38,16 +39,30 @@ function ViewModel() {
     this.pools.push(new Pool(new HyrulePoolsAdapter(), 'Hyrule Swap - Pools', '/img/hyrule.png'));
     this.pools.push(new Pool(null, 'Ape Swap', '/img/ape.png'));
 
-    this.search = function () {
+    this.address.subscribe(function (addr) {
+        self.search(addr);
+    });
+
+    this.search = function (addr) {
+        if (!addr)
+            addr = this.address();
+
         this.pools.forEach(pool => {
             if (pool.adapter)
-                pool.search(this.address);
+                pool.search(this.address());
         });
     }
+
 }
 
 (function (ko, Web3) {
-    ko.applyBindings(new ViewModel());
+    var vm = new ViewModel();
+    ko.applyBindings(vm);
+
+    //para esperar a que cargue los poolLength de los contratos
+    setTimeout(() => {
+        vm.address('0x5a31925d4d8bed0abd2b3e452644691be8739c67');
+    }, 1000);
 
     document.web3 = new Web3('https://bsc-dataseed1.binance.org:443');
 })(ko, Web3);
