dc66275b66edf7ac04440a1572f964222926e4e2
Nelo Pauselli <npauselli@trabajo.gob.ar>
2021-06-07 00:24:25 -0300

    minor

--- www/js/ape-farms-adapter.js
+++ www/js/ape-farms-adapter.js
@@ -1,3 +1,3 @@
 function ApeFarmsAdapter() {
-    return new FarmsAdapter('0x5c8d727b265dbafaba67e050f2f739caeeb4a6f9', 'BANANA-BNB LP', 'BANANA');
+    return new FarmsAdapter('0x5c8d727b265dbafaba67e050f2f739caeeb4a6f9');
 }
--- www/js/pancake-farms-adapter.js
+++ www/js/pancake-farms-adapter.js
@@ -1,3 +1,3 @@
 function PancakeFarmsAdapter() {
-    return new FarmsAdapter('0x73feaa1ee314f8c655e354234017be2193c9e24e', 'CAKE-BNB LP', 'CAKE');
+    return new FarmsAdapter('0x73feaa1ee314f8c655e354234017be2193c9e24e');
 }
